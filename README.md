f9f0ac3b1147eaf3f31bfe1f7363266a49598d6c
LabdhiBora <72147318+LabdhiBora@users.noreply.github.com>
2020-10-09 12:52:40 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Output link :https://labdhibora.github.io/Gcso/
+# p5.play-boilerplate
+Boiler plate for p5.play
--- sketch.js
+++ sketch.js
@@ -5,34 +5,46 @@ function setup() {
   line=createSprite(50,280,4000,10);
   line.shapeColor="white";
   car =createSprite(50,350,50,30);
-  car.shapeColor="red";
-  speed=random(180,280);
+ 
+  speed=random(55,90);
   weight=random(400,1500);
   
-  car.velocityX=speed;
+  car.velocityX=10;
 
-  car2=createSprite(50,220,50,30);
-  line=createSprite(50,150,4000,10);
-  line.shapeColor="white";
-  car2.shapeColor="green";
-  speed2=random(100,150);
-  car2.velocityX=speed2;
+  // car2=createSprite(50,220,50,30);
+  // line=createSprite(50,150,4000,10);
+  // line.shapeColor="white";
+  
+  // speed2=random(100,150);
+  // car2.velocityX=speed2;
 
-  car3=createSprite(50,70,50,30);
-  car3.shapeColor="yellow";
-  speed3=random(10,100);
-  car3.velocityX=speed3;
+  // car3=createSprite(50,70,50,30);
+  // car3.shapeColor="yellow";
+  // speed3=random(10,100);
+  // car3.velocityX=speed3;
 
   wall=createSprite(1500,200,60,height);
 }
 
 function draw() {
   background(0);  
- 
+ if (wall.x-car.x<(car.width+wall.width)/2){
+   car.velocityX=0
+   var deformation=(0.5 *weight *speed *speed)/22509
+   if(deformation>180){
+     car.shapeColor="red";
+   }
+   if (deformation<180 &&  deformation>100){
+     car.shapeColor="yellow"
+   }
+   if(deformation<100){
+     car.shapeColor="green";
+   }
+ }
 
   drawSprites();
   car.collide(wall);
-  car2.collide(wall);
-  car3.collide(wall);
+  // car2.collide(wall);
+  // car3.collide(wall);
 
 }
